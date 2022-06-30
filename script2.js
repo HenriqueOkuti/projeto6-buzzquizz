05f93ae95db7fb9654f392edb0d6dd48b6b083ae
Brunna Serafina <brunnaserafina@gmail.com>
2022-06-29 21:07:41 -0300

    Informações básicas e layout perguntas quizz (criação do quizz)

---
+++ script2.js
@@ -0,0 +1,48 @@
+let titulo;
+let tituloOk;
+let imagem;
+let imagemOk;
+let qtdPerguntas;
+let qtdPerguntasOk;
+let qtdNiveis;
+let qtdNiveisOk;
+
+function prosseguirParaCriarPerguntas() {
+    titulo = document.querySelector(".titulo-quizz").value;
+    tituloOk = (titulo.length >= 20  && titulo.length <= 65);
+
+    imagem = document.querySelector(".imagem-quizz").value;
+    imagemOk = (conferirUrl(imagem) === true); 
+
+    qtdPerguntas = document.querySelector(".qtd-perguntas").value;
+    qtdPerguntasOk = (qtdPerguntas >= 3); //quantidade de perguntas daqui irá adicionar perguntas da próxima página
+
+    qtdNiveis = document.querySelector(".qtd-niveis").value;
+    qtdNiveisOk = (qtdNiveis >= 2);
+
+    if (tituloOk && imagemOk && qtdPerguntasOk && qtdNiveisOk){
+        document.querySelector(".informacoes-basicas").classList.add("escondido");
+        document.querySelector(".criar-perguntas").classList.remove("escondido");
+    } else {
+        alert ("Insira os dados corretamente!")
+    }
+}
+
+function conferirUrl(img){
+    try {
+        let url = new URL(img)
+        return true;
+    } catch(err) {
+        return false;
+    }
+}
+
+
+
+
+
+
+
+
+
+
